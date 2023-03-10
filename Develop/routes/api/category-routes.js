c958e4813c0274a0fa64313d9db6b2076f0a229c
xAsdfaf <georgejordanjr8@gmail.com>
2023-03-10 12:21:40 -0600

    finished category-route.js... no testing done yet, may be buggy

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,24 +5,85 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll().then((categoryList) => {
+    res.json(categoryList);
+  });
   // be sure to include its associated Products
+  Product.findAll().then((productList) => {
+    res.json(productList);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const cateData = Category.findByPk(req.params.id);
+    if (!cateData) {
+      res.status(404).json({ message: 'No category associated with this ID!' });
+      return;
+    }
+      res.status(200).json(cateData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // be sure to include its associated Products
+  try {
+    const prodData = Category.findByPk(req.params.id);
+    if (!prodData) {
+      res.status(404).json({ message: 'No category associated with this ID!' });
+      return;
+    }
+      res.status(200).json(prodData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const cateData = User.create(req.body);
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const cateData = User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+      individualHooks: true
+    });
+    if (!cateData[0]) {
+      res.status(404).json({ message: 'No category associated with this id!' });
+      return;
+    }
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const cateData = User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!cateData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
